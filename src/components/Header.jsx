15d898e8d789b8acc05fd3b63d40dcec49fe888b
Mingkuan Yan <mingkuany@gmail.com>
2024-03-15 22:27:52 -0400

    Switch button between plan and track working!

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "../styles/header.module.scss";
 import logo from "../assets/car.svg";
 import account from "../assets/account.svg";
@@ -5,7 +6,15 @@ import GoPin from "./GoPin";
 
 export default function Header() {
   let loggedIn = true;
-  let trackSelected = true;
+  const [trackSelected, setTrackSelected] = useState(true);
+
+  const planClicked = () => {
+    setTrackSelected(false);
+  };
+
+  const trackClicked = () => {
+    setTrackSelected(true);
+  };
 
   return (
     <div className={styles.headerContainer}>
@@ -16,23 +25,29 @@ export default function Header() {
       <div className={styles.headerRightContainer}>
         {loggedIn ? (
           <div className={styles.toggleContainer}>
-            <div className={styles.planBtn}>
+            <button className={styles.planBtn} onClick={planClicked}>
               <GoPin width="2.6rem" go={true} blue={trackSelected} />
               <p
                 className={trackSelected ? styles.unselected : styles.selected}
               >
                 Plan
               </p>
-            </div>
-            <div className={styles.trackBtn}>
+            </button>
+            <button className={styles.trackBtn} onClick={trackClicked}>
               <GoPin width="2.6rem" go={false} blue={!trackSelected} />
               <p
                 className={trackSelected ? styles.selected : styles.unselected}
               >
                 Track
               </p>
-            </div>
-            <div className={styles.animation}></div>
+            </button>
+            <div
+              className={styles.animation}
+              style={{
+                left: trackSelected ? "9rem" : "0",
+                width: trackSelected ? "11rem" : "10rem",
+              }}
+            ></div>
           </div>
         ) : (
           <div className={styles.login}>Login</div>
