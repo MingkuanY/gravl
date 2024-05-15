2ae57e33daa98efed381755e933b821ede42ea71
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-05-15 16:07:23 -0700

    Created next js app

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// Dashboard
+
+import styles from "@/app/page.module.scss";
+
+export default function Dashboard() {
+  return (
+    <main className={styles.main}>
+      <h1>Dashboard</h1>
+    </main>
+  );
+}
