1b1873cb6db1c9095ea9e1d5a0c986bb55d654c5
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-06-21 11:20:19 -0700

    Completed loading page

--- src/app/redirect/page.tsx
+++ src/app/redirect/page.tsx
@@ -1,16 +1,22 @@
+// An temporary route to handle whether the User goes to onboarding after they sign in with Google.
+
 import { redirect } from "next/navigation";
 import { getUser } from "@/lib/user";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import Loading from "@/components/pages/Loading";
 
 export default async function Redirect() {
   const session = await getServerSession(authOptions);
 
+  // Gets the user associated with the current session
   const user = await getUser(session?.user.email);
   if (!user?.username) {
+    // Redirects them to onboarding if they lack a username
     redirect("/?ob=true");
   }
+  // Redirect them to their dashboard
   redirect(`/${user.username}`);
 
-  return <>Loading</>;
+  return <Loading />;
 }
