f7b14abca9baab44fde36826bc58300676308d44
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-07-18 21:42:26 -0700

    jwt refresh token code runs but with errors and still slow

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
-import { NextAuthOptions } from "next-auth";
-import { type User } from "next-auth";
+import { Account, NextAuthOptions, Profile, Session, User } from "next-auth";
+import { JWT } from "next-auth/jwt";
 import GoogleProvider from "next-auth/providers/google";
 
 const prisma = new PrismaClient();
@@ -15,79 +15,71 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token, account }) {
-      console.log("jwt works");
+    async jwt({
+      token,
+      user,
+      account,
+      profile,
+    }: {
+      token: JWT;
+      user?: User;
+      account?: Account | null;
+      profile?: Profile;
+    }) {
       if (account) {
-        console.log("ACCOUNT! WE HAVE AN ACCOUNT BLESSED LORD WE DO");
-        // First login, save the `access_token`, `refresh_token`, and other
-        // details into the JWT
-
-        const userProfile: User = {
-          // @ts-ignore
-          id: token.sub,
-          // @ts-ignore
-          name: profile?.name,
-          // @ts-ignore
-          email: profile?.email,
-          image: token?.picture,
+        // First login, save the access token and user profile in the token
+        token.access_token = account.access_token!;
+        token.refresh_token = account.refresh_token!;
+        token.expires_at = account.expires_at!;
+        token.user = {
+          id: profile?.sub ?? "",
+          name: profile?.name ?? "",
+          email: profile?.email ?? "",
+          image: profile?.image ?? "",
         };
-
-        return {
-          access_token: account.access_token,
-          expires_at: account.expires_at,
-          refresh_token: account.refresh_token,
-          user: userProfile,
-        };
-      } else if (Date.now() < token.expires_at * 1000) {
-        // Subsequent logins, if the `access_token` is still valid, return the JWT
+      } else if (Date.now() < (token.expires_at ?? 0) * 1000) {
+        // Subsequent logins, if the access token is still valid, return the token
         return token;
       } else {
-        // Subsequent logins, if the `access_token` has expired, try to refresh it
+        // Subsequent logins, if the access token has expired, try to refresh it
         if (!token.refresh_token) throw new Error("Missing refresh token");
 
         try {
-          // The `token_endpoint` can be found in the provider's documentation. Or if they support OIDC,
-          // at their `/.well-known/openid-configuration` endpoint.
-          // i.e. https://accounts.google.com/.well-known/openid-configuration
           const response = await fetch("https://oauth2.googleapis.com/token", {
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: new URLSearchParams({
-              client_id: process.env.AUTH_GOOGLE_ID!,
-              client_secret: process.env.AUTH_GOOGLE_SECRET!,
+              client_id: process.env.GOOGLE_CLIENT_ID!,
+              client_secret: process.env.GOOGLE_CLIENT_SECRET!,
               grant_type: "refresh_token",
               refresh_token: token.refresh_token!,
             }),
             method: "POST",
           });
 
-          const responseTokens = await response.json();
+          const refreshedTokens = await response.json();
 
-          if (!response.ok) throw responseTokens;
+          if (!response.ok) throw refreshedTokens;
 
-          return {
-            // Keep the previous token properties
-            ...token,
-            access_token: responseTokens.access_token,
-            expires_at: Math.floor(
-              Date.now() / 1000 + (responseTokens.expires_in as number)
-            ),
-            // Fall back to old refresh token, but note that
-            // many providers may only allow using a refresh token once.
-            refresh_token: responseTokens.refresh_token ?? token.refresh_token,
-          };
+          token.access_token = refreshedTokens.access_token!;
+          token.expires_at = Math.floor(
+            Date.now() / 1000 + refreshedTokens.expires_in!
+          );
+          token.refresh_token =
+            refreshedTokens.refresh_token ?? token.refresh_token;
         } catch (error) {
           console.error("Error refreshing access token", error);
-          // The error property can be used client-side to handle the refresh token error
-          return { ...token, error: "RefreshAccessTokenError" as const };
+          token.error = "RefreshAccessTokenError";
         }
       }
+      return token;
     },
-    async session({ session, token }) {
-      if (token?.user && session) {
-        // @ts-ignore
-        session.user = token.user as User;
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (token?.user) {
+        session.user = token.user;
       }
-
+      if (token?.error) {
+        session.error = token.error;
+      }
       return session;
     },
   },
@@ -98,6 +90,12 @@ export const authOptions: NextAuthOptions = {
 declare module "next-auth" {
   interface Session {
     error?: "RefreshAccessTokenError";
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image: string;
+    };
   }
 }
 
@@ -106,6 +104,12 @@ declare module "next-auth/jwt" {
     access_token: string;
     expires_at: number;
     refresh_token: string;
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image: string;
+    };
     error?: "RefreshAccessTokenError";
   }
 }
