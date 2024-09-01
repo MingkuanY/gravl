134202d78e9a35db6b5a293e7fccd39768aa5fca
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-09-01 15:24:10 -0400

    Shows "add a location" and "add a bio" only to the current user

--- src/components/dashboard/Profile.tsx
+++ src/components/dashboard/Profile.tsx
@@ -45,8 +45,14 @@ export default function Profile({
             </div>
           )}
         </div>
-        <p className={styles.location}>{user!.location || "Add a location"}</p>
-        <p className={styles.bio}>{user!.bio || "Add a bio"}</p>
+        {(user!.location || mode === "USER") && (
+          <p className={styles.location}>
+            {user!.location || "Add a location"}
+          </p>
+        )}
+        {(user!.bio || mode === "USER") && (
+          <p className={styles.bio}>{user!.bio || "Add a bio"}</p>
+        )}
         {mode === "FRIEND" && (
           <button className={styles.unfriendBtn} onClick={handleUnfriend}>
             Unfriend
