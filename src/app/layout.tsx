cffad10f3ddb25bf456101dc8b82ea432a072df1
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-09-24 23:55:27 -0400

    Hooked up with Google Maps Autocomplete API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,12 @@ export default function RootLayout({
   return (
     <SessionWrapper>
       <html lang="en">
+        <head>
+          <script
+            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`}
+            defer
+          ></script>
+        </head>
         <body className={josefinSans.className}>{children}</body>
       </html>
     </SessionWrapper>
--- src/components/log/DirectionsInput.tsx
+++ src/components/log/DirectionsInput.tsx
@@ -1,15 +1,89 @@
 import styles from "../../styles/directionsinput.module.scss";
 import Icon from "../icons/Icon";
 import classnames from "classnames";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import useOnclickOutside from "react-cool-onclickoutside";
+
+type Suggestion = {
+  description: string;
+  place_id: string;
+  structured_formatting: {
+    main_text: string;
+    secondary_text: string;
+  };
+};
 
 export default function DirectionsInput() {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    callbackName: "YOUR_CALLBACK_NAME",
+    requestOptions: {
+      /* Define search scope here */
+    },
+    debounce: 300,
+  });
+  const ref = useOnclickOutside(() => {
+    // When the user clicks outside of the component, we can dismiss
+    // the searched suggestions by calling this method
+    clearSuggestions();
+  });
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // Update the keyword of the input element
+    setValue(e.target.value);
+  };
+
+  const handleSelect =
+    ({ description }: Suggestion) =>
+    () => {
+      // When the user selects a place, we can replace the keyword without request data from API
+      // by setting the second parameter to "false"
+      setValue(description, false);
+      clearSuggestions();
+
+      // Get latitude and longitude via utility functions
+      getGeocode({ address: description }).then((results) => {
+        const { lat, lng } = getLatLng(results[0]);
+        console.log("📍 Coordinates: ", { lat, lng });
+      });
+    };
+
+  const renderSuggestions = () =>
+    data.map((suggestion) => {
+      const {
+        place_id,
+        structured_formatting: { main_text, secondary_text },
+      } = suggestion;
+
+      return (
+        <li key={place_id} onClick={handleSelect(suggestion)}>
+          <strong>{main_text}</strong> <small>{secondary_text}</small>
+        </li>
+      );
+    });
+
   return (
     <div className={styles.container}>
-      <div className={styles.inputContainer}>
+      <div className={styles.inputContainer} ref={ref}>
         <div className={classnames(styles.icon, styles.car)}>
           <Icon type="car" fill="#319fff" />
         </div>
-        <input type="text" placeholder="from" />
+        <input
+          type="text"
+          placeholder="from"
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+        />
+        {status === "OK" && <ul>{renderSuggestions()}</ul>}
       </div>
       <div className={styles.dotContainer}>
         <div></div>
