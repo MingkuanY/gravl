0dbc3bf93270db34c708cec40800c80400ad8719
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-09-28 22:11:00 -0400

    Made google maps api key accessible in browser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
       <html lang="en">
         <head>
           <script
-            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places`}
+            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
             defer
           ></script>
         </head>
