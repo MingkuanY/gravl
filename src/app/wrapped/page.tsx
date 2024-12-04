6282834149053e0b67cd07c74537e44d05bd967b
Mingkuan Yan <86630468+MingkuanY@users.noreply.github.com>
2024-12-04 17:29:10 -0500

    Styled gravl.org/wrapped

--- src/app/wrapped/page.tsx
+++ src/app/wrapped/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import SignUpButton from '@/components/landing/SignUpButton'
+import styles from '../../styles/wrapped.module.scss'
+import Header from '@/components/header/Header'
 
-export default function Wrapped() {
+export default function WrappedPage() {
   return (
-    <div>Wrapped</div>
+    <>
+      <Header />
+      <div className={styles.container}>
+        <h1>2024 Travel Wrapped</h1>
+        <h2>See where you've been this year.</h2>
+        <SignUpButton />
+      </div>
+    </>
   )
 }
